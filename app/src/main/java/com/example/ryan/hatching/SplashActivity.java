ad40632e7bcdfcca511741c7c0507989ae6ef4d0
liushixunryan <996822526@qq.com>
2019-05-26 14:59:57 +0800

    实现了进入界面的完成,第一次进入将有三秒的广告,第二次进入则没有

---
+++ app/src/main/java/com/example/ryan/hatching/SplashActivity.java
@@ -0,0 +1,49 @@
+package com.example.ryan.hatching;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    //下载完第一次进入会有3秒的广告,第二次则不会有
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        SharedPreferences sharedPreferences=getSharedPreferences("app",0);
+        Boolean isFirst=sharedPreferences.getBoolean("isFirst",true);
+
+        if (isFirst){
+            handler.postDelayed(runnable,3000);
+        }
+        else {
+            handler.postDelayed(runnable,0);
+        }
+        sharedPreferences.edit().putBoolean("isFirst",false).commit();
+    }
+    Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    };
+    Runnable runnable=new Runnable() {
+        @Override
+        public void run() {
+            Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
+    }
+}
