7322bfc27962537e67e86561e3b8a9b50040d4ec
liushixunryan <996822526@qq.com>
2019-06-04 13:35:47 +0800

    首页面的侧拉栏

--- app/src/main/java/com/example/ryan/hatching/MainActivity.java
+++ app/src/main/java/com/example/ryan/hatching/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.ryan.hatching;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout drawerLayout;
+    private ImageView userhead;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userhead=findViewById(R.id.userhead);
+        drawerLayout=findViewById(R.id.drawer_layout);
+
+        userhead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
     }
 }
